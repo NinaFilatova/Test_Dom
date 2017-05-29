3c84674b051fcc7d914194361cff9c0d8751975d
NinaFilatova <nina.filatova.a@gmail.com>
2017-05-29 18:03:02 +0300

    созданы вспомогательные методы и классы
    удачно пройденый тест

--- dom_test.py
+++ dom_test.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
 import unittest
+from group import Group
 
 
 def is_alert_present(wd):
@@ -21,7 +22,7 @@ class dom_test(unittest.TestCase):
         self.Open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.Open_groups_page(wd)
-        self.create_group(wd, name="YJdfz uheggf",header= "Yjdfz uheggf",footer= "YJdfz uheggf")
+        self.create_group(wd, Group (name="YJdfz uheggf",header= "Yjdfz uheggf",footer= "YJdfz uheggf"))
         self.return_to_groups_page(wd)
         self.logout(wd)
 
@@ -31,18 +32,18 @@ class dom_test(unittest.TestCase):
     def return_to_groups_page(self, wd):
         wd.find_element_by_link_text("group page").click()
 
-    def create_group(self, wd, name, header, footer):
+    def create_group(self, wd, group):
         # init group creation
         wd.find_element_by_name("group_name").click()
         # fill group firm
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(name)
+        wd.find_element_by_name("group_name").send_keys(group.name)
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(header)
+        wd.find_element_by_name("group_header").send_keys(group.header)
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(footer)
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creacion
         wd.find_element_by_name("submit").click()
 
