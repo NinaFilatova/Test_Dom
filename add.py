aa566bfc4d7ca52aca3d299437bb6f7f3e22121d
NinaFilatova <nina.filatova.a@gmail.com>
2017-05-30 17:07:37 +0300

    Создан вспомогательный класс
    тест пройден

---
+++ add.py
@@ -0,0 +1,24 @@
+
+class Add:
+    def __init__(self, firstname, middlename, lastname, nickname, title, company, address, home, mobile, work, fax,
+                 email, email2, email3, homepage, byear, ayear, address2, phone2, notes):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.home = home
+        self.mobile = mobile
+        self.work = work
+        self.fax = fax
+        self.email = email
+        self.email2 = email2
+        self.email3 = email3
+        self.homepage = homepage
+        self.byear = byear
+        self.ayear = ayear
+        self.address2 = address2
+        self.phone2 = phone2
+        self.notes = notes
