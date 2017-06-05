ae2dadf09c19694e99932fba9fd7e0bcf01e729a
NinaFilatova <nina.filatova.a@gmail.com>
2017-06-05 16:57:20 +0300

    Создан класс помощник для контактов

--- test/test_contact_creating.py
+++ test/test_contact_creating.py
@@ -14,10 +14,10 @@ def app(request):
 
 def test_add_test(app):
     app.session.login(username="admin", password="secret")
-    app.Init_add_creation(Contact(firstname="eyuyh", middlename="jknnijmn", lastname="akjkjkkjkj", nickname="asdad",
-                               title="Rjhghg", company="kjhjkhksjdf", address="sfdlkjfkjkj",
-                               home="sdfsfsd", mobile="sdfsdfsd", work="fsdfffffffffffffffff", fax="fsdfsdfsfds",
-                               email="fsdfdsfds", email2="fsdfsdfd", email3="fdsfsdfs", homepage="adfsadf", byear="4323",
-                               ayear="rwew", address2="rqwgxdgddgsgsg", phone2="dgsgsdgds", notes="gfdfgfd"))
-    app.init_add_creation()
+    app.contact.creation(Contact(firstname="eyuyh", middlename="jknnijmn", lastname="akjkjkkjkj", nickname="asdad",
+                                 title="Rjhghg", company="kjhjkhksjdf", address="sfdlkjfkjkj",
+                                 home="sdfsfsd", mobile="sdfsdfsd", work="fsdfffffffffffffffff", fax="fsdfsdfsfds",
+                                 email="fsdfdsfds", email2="fsdfsdfd", email3="fdsfsdfs", homepage="adfsadf", byear="4323",
+                                 ayear="rwew", address2="rqwgxdgddgsgsg", phone2="dgsgsdgds", notes="gfdfgfd"))
+    app.contact.init_add_creation()
     app.session.logout()
