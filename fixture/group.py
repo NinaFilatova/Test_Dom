bf8a47cadbcb5fd1d72e15b08ea949d1a7b8b471
NinaFilatova <nina.filatova.a@gmail.com>
2017-06-05 17:54:50 +0300

    Фикструра для всех тестов

--- fixture/group.py
+++ fixture/group.py
@@ -25,6 +25,14 @@ class GroupHelper:
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
 
+    def delete_first_group(self):
+        wd = self.app.wd
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # submit deletion
+        wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
+
     def open_groups_page(self):
         wd = self.app.wd
         wd.find_element_by_name("new").click()
--- test/test_group_creating.py
+++ test/test_group_creating.py
@@ -1,14 +1,7 @@
 # -*- coding: utf-8 -*-
 
-import pytest
 from model.group import Group
-from fixture.application import Application
 
-@ pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 def test_dom_test(app):
       app.session.login(username="admin", password="secret")
