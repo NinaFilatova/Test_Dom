911998c38f07b2b41b3b79ff77e63e6fa6eb5bcf
NinaFilatova <nina.filatova.a@gmail.com>
2017-06-13 15:57:56 +0300

    тесты

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -1,61 +1,22 @@
 from model.contact import Contact
 
 def test_modify_contact_firstname(app):
+    if app.contact.count() == 0:
+        app.contact.creation(Contact(firstname="test", middlename="test", lastname="test", nickname="test",
+                                 title="test", company="test", address="test",
+                                 home="test", mobile="test", work="test", fax="test",
+                                 email="test", email2="test", email3="test", homepage="test", byear="test",
+                                 ayear="test", address2="test", phone2="test", notes="test"))
+        app.contact.init_add_creation()
     app.contact.modify_first_contact(Contact(firstname="New contact"))
-
-def test_modify_contact_middlename(app):
-    app.contact.modify_first_contact(Contact(middlename="New contact"))
-
-def test_modify_contact_lastname(app):
-    app.contact.modify_first_contact(Contact(lastname="New contact"))
-
-def test_modify_contact_nickname(app):
-    app.contact.modify_first_contact(Contact(nickname="New contact"))
-
-def test_modify_contact_title(app):
-    app.contact.modify_first_contact(Contact(title="New contact"))
-
-def test_modify_contact_company(app):
-    app.contact.modify_first_contact(Contact(company="New contact"))
-
-def test_modify_contact_address(app):
-    app.contact.modify_first_contact(Contact(address="New contact"))
-
-def test_modify_contact_home(app):
-    app.contact.modify_first_contact(Contact(home="New contact"))
-
-def test_modify_contact_mobile(app):
-    app.contact.modify_first_contact(Contact(mobile="New contact"))
-
-def test_modify_contact_work(app):
-    app.contact.modify_first_contact(Contact(work="New contact"))
-
-def test_modify_contact_fax(app):
-    app.contact.modify_first_contact(Contact(fax="New contact"))
-
-def test_modify_contact_email(app):
-    app.contact.modify_first_contact(Contact(email="New contact"))
-
-def test_modify_contact_email2(app):
-    app.contact.modify_first_contact(Contact(email2="New contact"))
-
-def test_modify_contact_email3(app):
-    app.contact.modify_first_contact(Contact(email3="New contact"))
-
-def test_modify_contact_homepage(app):
-    app.contact.modify_first_contact(Contact(homepage="New contact"))
-
-def test_modify_contact_byear(app):
-    app.contact.modify_first_contact(Contact(byear="New contact"))
-
-def test_modify_contact_ayear(app):
-    app.contact.modify_first_contact(Contact(ayear="New contact"))
-
-def test_modify_contact_address2(app):
-    app.contact.modify_first_contact(Contact(address2="New contact"))
-
-def test_modify_contact_phone2(app):
-    app.contact.modify_first_contact(Contact(phone2="New contact"))
+    app.contact.delete_first_contact()
 
 def test_modify_contact_notes(app):
+    if app.contact.count() == 0:
+        app.contact.creation(Contact(firstname="test", middlename="test", lastname="test", nickname="test",
+                                     title="test", company="test", address="test",
+                                     home="test", mobile="test", work="test", fax="test",
+                                     email="test", email2="test", email3="test", homepage="test", byear="test",
+                                     ayear="test", address2="test", phone2="test", notes="test"))
+        app.contact.init_add_creation()
     app.contact.modify_first_contact(Contact(notes="New contact"))
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,10 +2,19 @@ from model.group import Group
 
 
 def test_modify_group_name(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
     app.group.modify_first_group(Group(name="New group"))
-
-def test_modify_group_header(app):
-     app.group.modify_first_group(Group(header="New header"))
+    app.group.delete_first_group()
 
 def test_modify_group_footer(app):
+    if app.group.count() == 0:
+        app.group.create(Group(footer="test"))
     app.group.modify_first_group(Group(footer="New footer"))
+    app.group.delete_first_group()
+
+def test_modify_group_header(app):
+    if app.group.count() == 0:
+        app.group.create(Group(header="test"))
+    app.group.modify_first_group(Group(header="New footer"))
+    app.group.delete_first_group()
