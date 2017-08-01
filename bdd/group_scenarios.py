ae89b7803122d09dd060ec9ffbeb2e951d78426f
NinaFilatova <nina.filatova.a@gmail.com>
2017-08-01 17:42:30 +0300

    [kfv

---
+++ bdd/group_scenarios.py
@@ -0,0 +1,10 @@
+from pytest_bdd import scenario
+from .group_steps import *
+
+@scenario('groups.feature', 'Add new group')
+def test_add_new_group():
+    pass
+
+@scenario('groups.feature', 'Delete a group')
+def test_delete_group():
+    pass
--- test/test_db_matches_ui.py
+++ test/test_db_matches_ui.py
@@ -9,3 +9,4 @@ def test_group_list(app, db):
 
     db_list = map(clean, db.get_group_list())
     assert sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
+#
